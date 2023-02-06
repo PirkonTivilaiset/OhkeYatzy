51802ad92e48b24da86f6df624fa7233925e7a0e
xirtoth <henry.heino11@gmail.com>
2023-02-07 00:10:04 +0200

    Noppa luokkaan randomi stattiseksi
    Muutettiin vielä Noppa luokaan arvonta staattiseksi

--- Yatzy/Noppa.cs
+++ Yatzy/Noppa.cs
@@ -2,12 +2,13 @@ public class Noppa
 {
     public bool lukittu { get; set; }
     public int arvo { get; set; }
-    private readonly Random arvonta = new Random();
+    private static readonly Random arvonta = new Random();
 
     public void Heitto()
     {
         if (!lukittu)
         {
+            //Sets arvo to a random integer that is within specified range.
             arvo = arvonta.Next(1, 7);
         }
     }
